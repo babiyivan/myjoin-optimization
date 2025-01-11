ee2b7e268501414b39daae964bf532bdff3c5d74
Marijana Petojevic <marijana.petojevic@tuwien.ac.at>
2025-01-11 22:42:55 +0100

    initial myjoin implementation in c++

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # EP-Group-12
+
+## Original Implementation
+
+This implementation serves as the baseline reference, without any efficiency optimizations. It is intended as a starting point for further improvements.
+
+### Building the Program
+1. Navigate to the `./original_implementation` directory.
+2. Run the following command to build the program:
+   ```bash
+   make
+   ```
+
+### Cleaning the Build
+To clean up all generated files (object files and executables) and reset repository to its original state, run:
+```bash
+make clean
+```
+
+### Testing Environment
+This implementation has been tested on the `g0` server. To replicate the tests:
+1. Clone the repository to the `g0` server.
+2. Build the executable as described above.
+3. Navigate to the directory `/localtmp/efficient24`:
+   ```bash
+   cd /localtmp/efficient24
+   ```
+
+### Comparing Output
+You can compare the program's output with the expected results using the following commands:
+
+#### Small Data Set:
+```bash
+~/EP-Group-12/original_implementation/myjoin a.csv b.csv c.csv d.csv | sort | diff - abcd.csv
+```
+
+#### Large Data Set:
+```bash
+~/EP-Group-12/original_implementation/myjoin f1.csv f2.csv f3.csv f4.csv | sort | diff - output.csv
+```
+
+### Local test
+
+Navigate to the `./original_implementation` and to execute the program with predefined test files form `test` subdirectory, run:
+
+```
+./myjoin test/file1.csv test/file2.csv test/file3.csv test/file4.csv
+```
---
+++ original_implementation/myjoin.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+
+using namespace std;
+
+vector<string> split(const string& line, char delimiter) {
+    vector<string> tokens;
+    stringstream ss(line);
+    string token;
+
+    while (getline(ss, token, delimiter)) {
+        tokens.push_back(token);
+    }
+
+    return tokens;
+}
+
+// Read a CSV file and return its contents as a vector of pairs
+vector<pair<string, string>> readFile(const string& filename) {
+    vector<pair<string, string>> data;
+    ifstream file(filename);
+    string line;
+
+    while (getline(file, line)) {
+        auto tokens = split(line, ',');
+        if (tokens.size() == 2) {
+            data.emplace_back(tokens[0], tokens[1]);
+        }
+    }
+
+    return data;
+}
+
+void my_join(const string& file1, const string& file2, const string& file3, const string& file4) {
+    auto data1 = readFile(file1);
+    auto data2 = readFile(file2);
+    auto data3 = readFile(file3);
+    auto data4 = readFile(file4);
+
+    multimap<string, string> map1(data1.begin(), data1.end());
+    multimap<string, string> map2(data2.begin(), data2.end());
+    multimap<string, string> map3(data3.begin(), data3.end());
+    multimap<string, string> map4(data4.begin(), data4.end());
+
+    for (const auto& [key1, value1] : map1) {
+        auto range2 = map2.equal_range(key1);
+        auto range3 = map3.equal_range(key1);
+
+        for (auto it2 = range2.first; it2 != range2.second; ++it2) {
+            for (auto it3 = range3.first; it3 != range3.second; ++it3) {
+                auto range4 = map4.equal_range(it3->second);
+
+                for (auto it4 = range4.first; it4 != range4.second; ++it4) {
+                    cout << it3->second << "," << key1 << "," << value1 << "," << it2->second << "," << it4->second << "\n";
+                }
+            }
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 5) {
+        cerr << "Usage: " << argv[0] << " <file1> <file2> <file3> <file4>\n";
+        return 1;
+    }
+
+    my_join(argv[1], argv[2], argv[3], argv[4]);
+
+    return 0;
+}
